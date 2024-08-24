5df986e77865ea838e6f87569ffa065b66716b3c
prem <premkumardanav@gmail.com>
2024-08-24 12:42:16 +0530

    Added create message dto

--- Nestjs-cli-project/messages/src/messages/messages.controller.ts
+++ Nestjs-cli-project/messages/src/messages/messages.controller.ts
@@ -1,12 +1,12 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-
+import { CreateMessageDto } from './dtos/create-message.dto';
 @Controller('messages')
 export class MessagesController {
   @Get()
   listMessages() {}
 
   @Post()
-  createMessages(@Body() body: any) {
+  createMessages(@Body() body: CreateMessageDto) {
     console.log(`boyd`, body);
   }
 
